1b6aaed478bca49a8cdf3e197b54473a407c1f87
ziming <308232015@qq.com>
2022-10-19 14:47:42 +0800

    feat: init repo

---
+++ pages/posts/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import Layout from "../../components/Layout"
+export default function FirstPost() {
+    return (
+        <>
+        <Layout>
+        <Head>
+            <title> posts</title>
+
+        </Head>
+        <h1>First Post</h1>
+        </Layout>
+
+        </>
+    )
+}
